5273b19a8f8fadb2d2857b6bd06b7a3815517da8
nibmangit <nibretumengaw@gmail.com>
2026-05-28 00:03:37 +0300

    feat: add the edit functionality of the task

--- src/components/tasks/TaskItem.jsx
+++ src/components/tasks/TaskItem.jsx
@@ -1,11 +1,40 @@
-function TaskItem({ task, deleteTask, toggleTask }) {
+import { useState } from "react";
+
+function TaskItem({
+  task,
+  deleteTask,
+  toggleTask,
+  editTask,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(task.title);
+
+  const handleEdit = () => {
+    if (!editedText.trim()) return;
+
+    editTask(task.id, editedText);
+    setIsEditing(false);
+  };
+
   return (
     <div className="flex justify-between items-center bg-gray-800 p-3 rounded">
-      <span
-        className={task.completed ? "line-through text-gray-400" : ""}
-      >
-        {task.title}
-      </span>
+      {isEditing ? (
+        <input
+          className="bg-black text-white px-2 py-1 rounded flex-1 mr-2"
+          value={editedText}
+          onChange={(e) => setEditedText(e.target.value)}
+        />
+      ) : (
+        <span
+          className={
+            task.completed
+              ? "line-through text-gray-400"
+              : ""
+          }
+        >
+          {task.title}
+        </span>
+      )}
 
       <div className="flex gap-2">
         <button
@@ -14,11 +43,23 @@ function TaskItem({ task, deleteTask, toggleTask }) {
         >
           ✓
         </button>
-       <button
-          className="bg-yellow-500 hover:bg-yellow-600 px-3 py-1 rounded text-white"
-        >
-          ✎
-        </button>
+
+        {isEditing ? (
+          <button
+            onClick={handleEdit}
+            className="bg-blue-500 px-3 py-1 rounded"
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsEditing(true)}
+            className="bg-yellow-500 hover:bg-yellow-600 px-3 py-1 rounded text-white"
+          >
+            ✎
+          </button>
+        )}
+
         <button
           onClick={() => deleteTask(task.id)}
           className="bg-red-500 px-2 py-1 rounded"
